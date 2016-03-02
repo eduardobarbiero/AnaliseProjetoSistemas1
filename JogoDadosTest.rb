04e31bc8546091bbe801ab7f7829c6da4ff3f5cd
eduardobarbiero <eduardombarbiero@gmail.com>
2016-03-02 20:27:11 -0300

    Criado uma classe para cada teste

--- JogoDadosTest.rb
+++ JogoDadosTest.rb
@@ -1,18 +1,20 @@
 require_relative "jogodados"
 require "test/unit"
 
-class JogoDadosTest < Test::Unit::TestCase
-
+class InfosTest < Test::Unit::TestCase
   def test_infos    
     assert_equal(true, (Array.new([1,2,3,4,5,6,7]) == JogoDados::Dado.infos))   
   end
-  
+end	
+
+class SorteioTest < Test::Unit::TestCase
   def test_sorteio      
     assert_equal(true, (JogoDados::Sortear.sortear > 0))   
-  end 
-  
+  end
+end	
+
+class JogoTest < Test::Unit::TestCase  
   def test_jogo
     assert_equal(true, JogoDados.jogar)     
   end
- 
 end
