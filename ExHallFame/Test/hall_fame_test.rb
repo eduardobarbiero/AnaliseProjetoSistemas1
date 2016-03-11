ef1a10efdfa5b2487acb918365b65210e27c50d3
Eduardo Mauricio Barbiero <eduardombarbiero@gmail.com>
2016-03-11 15:48:32 -0300

    sorte first 10.

--- ExHallFame/Test/hall_fame_test.rb
+++ ExHallFame/Test/hall_fame_test.rb
@@ -10,4 +10,25 @@ class HallFameTest < Test::Unit::TestCase
     assert_equal(1, player.length)
 	assert_equal(expected, player[0].nome)
   end
+  
+  def test_gamers_sort_first_10
+    hall = HallFame.new
+    players = set_players
+	sorted = HallFame.sort_first_10(players)
+    assert_nothing_raised((sorted[hall.length-1] > sorted[hall.length-2]))  
+  end
+  
+  private
+  
+  def set_players
+    hall = HallFame.new
+	i = -1
+    for i in 1..10
+	  player = HallFame::Player.new
+	  player.nome = "Jogador " + i.to_s
+	  player.point = Random.rand(100)
+	  hall.player << player
+	end	
+	hall.player
+  end	
 end	
